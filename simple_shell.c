e8e3e54226a380cafeb54d07906d29187bffa1fd
Marcel Plakolb <marcel.plakolb@liwest.at>
2019-10-25 09:44:43 +0200

    wip

--- simple_shell.c
+++ simple_shell.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
 
 int main(int argc, char const *argv[]) {
-  
+  int status;
+  if (fork() != 0) {
+    waitpid(-1, &status, 0);
+   }
+   else{
+     execvp(args[0], args);
+   }
   return 0;
 }
